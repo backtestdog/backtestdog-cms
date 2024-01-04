71aff46388beb261559406ca3dc5e3151b55d8cd
janlin002 <123davidbill@gmail.com>
2024-01-04 15:10:44 +0800

    fix: adjust pinia to composition API(#pinia)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>{{ counterStore.name }}</div>
+  <div>{{ count }}</div>
+  <button @click="increment">點擊加一</button>
 </template>
 
 <script setup>
 import { useCounterStore } from '@/store/counter'
+import { storeToRefs } from 'pinia'
 
 const counterStore = useCounterStore()
+
+const { count } = storeToRefs(counterStore)
+const { increment } = counterStore
 </script>
 
 <style></style>
--- src/store/counter.ts
+++ src/store/counter.ts
@@ -1,13 +1,16 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useCounterStore = defineStore('counter', {
-  state: () => ({ count: 0, name: 'Eduardo' }),
-  getters: {
-    doubleCount: (state) => state.count * 2
-  },
-  actions: {
-    increment() {
-      this.count++
-    }
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+
+  const doubleCount = () => {
+    return count.value * 2
   }
+
+  const increment = () => {
+    count.value++
+  }
+
+  return { count, doubleCount, increment }
 })
