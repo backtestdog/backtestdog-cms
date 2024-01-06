49f246c4ee5f5253a6ca268a8202b0b950536c1d
janlin002 <123davidbill@gmail.com>
2024-01-06 18:12:56 +0800

    feat: add type

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,11 +3,18 @@
   <button @click="increment">點擊加一</button>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { type Ref } from 'vue'
 import { useCounterStore } from '@/store/counter'
 import { storeToRefs } from 'pinia'
 
-const counterStore = useCounterStore()
+type CounterStore = {
+  count: Ref<number>
+  doubleCount: () => number
+  increment: () => number
+}
+
+const counterStore: CounterStore = useCounterStore()
 
 const { count } = storeToRefs(counterStore)
 const { increment } = counterStore
