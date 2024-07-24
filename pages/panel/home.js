b759c784d1db4a7372779fc3996ed203a3218c0e
ottoLiu <otto.ven.liu@gmail.com>
2024-07-24 15:40:03 +0800

    feat: template_init

---
+++ pages/panel/home.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Button from "janlab-ui";
+export default function MainPanel() {
+  return (
+    <div>
+      <h1>Control Panel</h1>
+      <Link href="/">Back to Home Page !</Link>
+    </div>
+  );
+}
